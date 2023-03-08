f10edaaf01237e98f5b3b6a6574b87963e6b13bb
thomasjshea <thomasjshea89@gmail.com>
2023-03-07 23:00:21 -0600

    added API calls to JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,55 @@
-// add text input to search area
-function addSearch() {
-let searchAreaEl = $('.search-area')
-// create an input element undear search area
-let inputEl = $('<input>')
-inputEl.attr('type','text')
-inputEl.attr('placeholder', 'Select a City')
-inputEl.attr('class','row text-area')
-// create a search button
-let searchBtnEl = $('<button>')
-searchBtnEl.attr('type','button')
-searchBtnEl.attr('class', 'btn btn-lg row search-btn')
-searchBtnEl.text('Search')
-// append input element to search area
-searchAreaEl.append(inputEl)
-searchAreaEl.append(searchBtnEl)
+let appID = '40fc403148f0bd5f81ba1d88009de8c2'
+
+function getCoordinates() {
+    let cityInput = $('#search-bar').val()
+    let geoURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityInput +'&limit=5&appid=' + appID
+    fetch(geoURL)
+    .then (function(response){
+        return response.json()
+    })
+    .then (function(data){
+        console.log(data)
+        let lat = data[0].lat
+        let lon = data[0].lon
+        function getWeather(){
+            let weatherURL = 'https://api.openweathermap.org/data/2.5/weather?lat='+ lat +'&lon='+ lon +'&units=imperial&appid='+ appID
+            console.log(weatherURL)
+            fetch(weatherURL)
+            .then(function(response){
+                return response.json()
+            })
+            .then(function(data){
+                console.log(data)
+                $('#city').html(data.name)
+                $('#weather-image').attr('src', 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png')
+            })
+        }
+        getWeather()
+        getForecast()
+    })
+
 }
 
-addSearch()
+
+
+let citySearches = []
+
+function showButtons() {
+    $('#recent-searches').empty()
+    for (let i = 0; i < citySearches.length; i++) {
+        let cityButton = $("<button>")
+        cityButton.addClass('cities btn mb-2')
+        cityButton.attr('data-name', citySearches[i])
+        cityButton.text(citySearches[i])
+        $('#recent-searches').append(cityButton)
+    }
+}
+
+$('#search-btn').click(function (event) {
+    // event.preventDefault()
+    let city = $('#search-bar').val().trim()
+    citySearches.push(city)
+    showButtons()
+    getCoordinates()
+})
+
