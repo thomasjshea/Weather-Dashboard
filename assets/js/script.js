851c4ad9b2220668deeb56dd316599e00e0ce289
thomasjshea <thomasjshea89@gmail.com>
2023-03-14 20:09:02 -0500

    updated 5 day forecast and made city buttons functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,35 @@ function getCoordinates() {
             })
             .then(function(data){
                 console.log(data)
-                $('#weather-image-1').attr('src','http://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png')
+                $('#weather-image-1').attr('src','http://openweathermap.org/img/w/' + data.list[5].weather[0].icon + '.png')
+                $('#date-1').html(dayjs().add(1,'day').format('MM/DD/YYYY'))
+                $('#temperature-1').html(data.list[5].main.temp_max)
+                $('#wind-1').html(data.list[5].wind.speed)
+                $('#humidity-1').html(data.list[5].main.humidity)
+
+                $('#weather-image-2').attr('src','http://openweathermap.org/img/w/' + data.list[13].weather[0].icon + '.png')
+                $('#date-2').html(dayjs().add(2,'day').format('MM/DD/YYYY'))
+                $('#temperature-2').html(data.list[13].main.temp_max)
+                $('#wind-2').html(data.list[13].wind.speed)
+                $('#humidity-2').html(data.list[13].main.humidity)
+
+                $('#weather-image-3').attr('src','http://openweathermap.org/img/w/' + data.list[21].weather[0].icon + '.png')
+                $('#date-3').html(dayjs().add(3,'day').format('MM/DD/YYYY'))
+                $('#temperature-3').html(data.list[21].main.temp_max)
+                $('#wind-3').html(data.list[21].wind.speed)
+                $('#humidity-3').html(data.list[21].main.humidity)
+
+                $('#weather-image-4').attr('src','http://openweathermap.org/img/w/' + data.list[29].weather[0].icon + '.png')
+                $('#date-4').html(dayjs().add(4,'day').format('MM/DD/YYYY'))
+                $('#temperature-4').html(data.list[29].main.temp_max)
+                $('#wind-4').html(data.list[29].wind.speed)
+                $('#humidity-4').html(data.list[29].main.humidity)
+
+                $('#weather-image-5').attr('src','http://openweathermap.org/img/w/' + data.list[37].weather[0].icon + '.png')
+                $('#date-5').html(dayjs().add(5,'day').format('MM/DD/YYYY'))
+                $('#temperature-5').html(data.list[37].main.temp_max)
+                $('#wind-5').html(data.list[37].wind.speed)
+                $('#humidity-5').html(data.list[37].main.humidity)
             })
         }
         getWeather()
@@ -47,7 +75,7 @@ function getCoordinates() {
 
 
 
-let citySearches = []
+var citySearches = []
 
 function showButtons() {
     $('#recent-searches').empty()
@@ -59,13 +87,39 @@ function showButtons() {
         $('#recent-searches').append(cityButton)
     }
 }
+showButtons()
 
-$('#search-btn').click(function (event) {
-    // event.preventDefault()
+function init() {
+    let storedCities = JSON.parse(localStorage.getItem('searches'))
+    if(storedCities !== null) {
+        citySearches = storedCities
+        showButtons()
+    }
+}
+
+function clearSearch(){
+    $('#search-bar').val('')
+}
+
+$('#search-btn').click(function () {
     let city = $('#search-bar').val().trim()
     citySearches.push(city)
-    localStorage.setItem('searches', citySearches)
+    localStorage.setItem('searches', JSON.stringify(citySearches))
     showButtons()
     getCoordinates()
+    clearSearch()
 })
+
+$(document).on('click', '.cities', function(){
+    let selectedCity = $(this).attr('data-name')
+    console.log(selectedCity)
+    $('#search-bar').val(selectedCity)
+    getCoordinates()
+    clearSearch()
+})
+
+init()
+
+
+
 
