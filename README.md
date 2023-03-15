393059eeced3012e69c179a8d995cecb6c023857
thomasjshea <thomasjshea89@gmail.com>
2023-03-14 20:32:28 -0500

    updated readme file

--- README.md
+++ README.md
@@ -1,103 +1,29 @@
-# 06 Server-Side APIs: Weather Dashboard
+# Weather Dashboard
 
-## Your Task
+## Description
 
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+This application is a Weather Dashboard. The user will type in a city, or select from a list of previously searched cities. The application will then output the current conditions for that city along with a 5 day forecast. As someone who travels and enjoys participating in outdoor activities, it is important for me to know the weather forcast so I can plan ahead. 
 
-Use the [5 Day Weather Forecast](https://openweathermap.org/forecast5) to retrieve weather data for cities. The base URL should look like the following: `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate.
+## Installation
 
-**Hint**: Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
+No installation is required to use this application. As a user, you will simply need to load the URL into your browser. 
 
-You will use `localStorage` to store any persistent data. For more information on how to work with the OpenWeather API, refer to the [Full-Stack Blog on how to use API keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys).
+## Usage
 
-## User Story
+To use this application, enter the URL in your browser. Type in a city name and click the search button. The application will display current conditions and a 5-day forecast. 
 
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
+Screenshots:
 
-## Acceptance Criteria
+![Dashboard Screenshot](/assets/Images/Dashboard-Screenshot.png)
 
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-```
+Deployed URL: https://thomasjshea.github.io/Weather-Dashboard/
 
-## Mock-Up
+## Credits
 
-The following image shows the web application's appearance and functionality:
+I completed this project with minimal assistance.
+I did recieve some input from classmate Ian Heap to clean up some minor functionality issues
 
-![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./Assets/06-server-side-apis-homework-demo.png)
+## License
 
-## Grading Requirements
-
-> **Note**: If a Challenge assignment submission is marked as “0”, it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
-
-This Challenge is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the above acceptance criteria plus the following:
-
-    * Uses the OpenWeather API to retrieve weather data.
-
-    * Uses `localStorage` to store persistent data.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the Challenge instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a readme describing the project.
-
-- - -
-© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
+This application uses the MIT License
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 let appID = '40fc403148f0bd5f81ba1d88009de8c2'
 
+// Everything is included under the getCoordinates function
 function getCoordinates() {
     let cityInput = $('#search-bar').val()
+    // the geocoding URL to obtain latitude and longitude 
     let geoURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityInput +'&limit=5&appid=' + appID
     fetch(geoURL)
     .then (function(response){
@@ -9,8 +11,10 @@ function getCoordinates() {
     })
     .then (function(data){
         console.log(data)
+        // gets the lat/lon data from the API
         let lat = data[0].lat
         let lon = data[0].lon
+        // getWeather function gets the current conditions from the API
         function getWeather(){
             let weatherURL = 'https://api.openweathermap.org/data/2.5/weather?lat='+ lat +'&lon='+ lon +'&units=imperial&appid='+ appID
             console.log(weatherURL)
@@ -20,6 +24,7 @@ function getCoordinates() {
             })
             .then(function(data){
                 console.log(data)
+                // Sets the inner HTML of each selection using jQuery
                 $('#city').html(data.name)
                 $('#date').html(dayjs().format('MM/DD/YYYY'))
                 $('#weather-image').attr('src', 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png')
@@ -28,6 +33,7 @@ function getCoordinates() {
                 $('#humidity').html(data.main.humidity)
             })
         }
+        // fetches the 5-day forecast from the API
         function getForecast() {
             let forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?lat='+ lat +'&lon='+ lon +'&units=imperial&appid='+ appID
             fetch(forecastURL)
@@ -36,30 +42,35 @@ function getCoordinates() {
             })
             .then(function(data){
                 console.log(data)
+                // Sets day 1 of the forecast
                 $('#weather-image-1').attr('src','http://openweathermap.org/img/w/' + data.list[5].weather[0].icon + '.png')
                 $('#date-1').html(dayjs().add(1,'day').format('MM/DD/YYYY'))
                 $('#temperature-1').html(data.list[5].main.temp_max)
                 $('#wind-1').html(data.list[5].wind.speed)
                 $('#humidity-1').html(data.list[5].main.humidity)
-
+                
+                // Sets day 2 of the forecast
                 $('#weather-image-2').attr('src','http://openweathermap.org/img/w/' + data.list[13].weather[0].icon + '.png')
                 $('#date-2').html(dayjs().add(2,'day').format('MM/DD/YYYY'))
                 $('#temperature-2').html(data.list[13].main.temp_max)
                 $('#wind-2').html(data.list[13].wind.speed)
                 $('#humidity-2').html(data.list[13].main.humidity)
 
+                // Sets day 3 of the forecast
                 $('#weather-image-3').attr('src','http://openweathermap.org/img/w/' + data.list[21].weather[0].icon + '.png')
                 $('#date-3').html(dayjs().add(3,'day').format('MM/DD/YYYY'))
                 $('#temperature-3').html(data.list[21].main.temp_max)
                 $('#wind-3').html(data.list[21].wind.speed)
                 $('#humidity-3').html(data.list[21].main.humidity)
 
+                // Sets day 4 of the forecast
                 $('#weather-image-4').attr('src','http://openweathermap.org/img/w/' + data.list[29].weather[0].icon + '.png')
                 $('#date-4').html(dayjs().add(4,'day').format('MM/DD/YYYY'))
                 $('#temperature-4').html(data.list[29].main.temp_max)
                 $('#wind-4').html(data.list[29].wind.speed)
                 $('#humidity-4').html(data.list[29].main.humidity)
 
+                // Sets day 5 of the forecast
                 $('#weather-image-5').attr('src','http://openweathermap.org/img/w/' + data.list[37].weather[0].icon + '.png')
                 $('#date-5').html(dayjs().add(5,'day').format('MM/DD/YYYY'))
                 $('#temperature-5').html(data.list[37].main.temp_max)
@@ -67,6 +78,7 @@ function getCoordinates() {
                 $('#humidity-5').html(data.list[37].main.humidity)
             })
         }
+        // calls the getWeather and getForecast function after the coordinates are fetched from the Geocoding API
         getWeather()
         getForecast()
     })
@@ -74,9 +86,9 @@ function getCoordinates() {
 }
 
 
-
+// sets an empty array to save past searches to
 var citySearches = []
-
+// Shows the city buttons and sets their attributes
 function showButtons() {
     $('#recent-searches').empty()
     for (let i = 0; i < citySearches.length; i++) {
@@ -89,6 +101,7 @@ function showButtons() {
 }
 showButtons()
 
+// initializes the page
 function init() {
     let storedCities = JSON.parse(localStorage.getItem('searches'))
     if(storedCities !== null) {
@@ -97,10 +110,13 @@ function init() {
     }
 }
 
+// clears the search input area
 function clearSearch(){
     $('#search-bar').val('')
 }
 
+// adds event listener to the search button
+// this function saves the city name to local storage, adds the city button, starts the getCoordinates function and clears the search bar
 $('#search-btn').click(function () {
     let city = $('#search-bar').val().trim()
     citySearches.push(city)
@@ -110,6 +126,8 @@ $('#search-btn').click(function () {
     clearSearch()
 })
 
+// adds event listener to the cities buttons
+// sets the search bar value to the selected city, calls the getCoordinates function for that city and clears the search bar
 $(document).on('click', '.cities', function(){
     let selectedCity = $(this).attr('data-name')
     console.log(selectedCity)
@@ -118,6 +136,7 @@ $(document).on('click', '.cities', function(){
     clearSearch()
 })
 
+// calls the init function
 init()
 
 
